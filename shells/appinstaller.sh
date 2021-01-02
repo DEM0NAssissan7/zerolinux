8963d3a1b6b797445a6c016d43e2fffea88e27e8
mawi <42104518+DEM0NAssissan7@users.noreply.github.com>
2021-01-02 15:28:52 -0600

    changed where extras is stored in /mnt

--- shells/appinstaller.sh
+++ shells/appinstaller.sh
@@ -7,33 +7,34 @@ echo
 read -p "Select which number to install [1,2,3]: " insnumber
 echo You have selected option $insnumber
 echo
-mkdir /mnt/tmp
-mkdir /mnt/tmp/install
-cp /tmp/zerolinux/extras/mawlspack-install.sh /mnt/tmp/install/
-cp /tmp/zerolinux/extras/aurapps.sh /mnt/tmp/install/
-chmod +x /mnt/tmp/install/mawlspack-install.sh
-chmod +x /mnt/tmp/install/aurapps.sh
+mkdir /mnt/home
+mkdir /mnt/home/install
+cp /tmp/zerolinux/extras/mawlspack-install.sh /mnt/home/install/
+cp /tmp/zerolinux/extras/aurapps.sh /mnt/home/install/
+chmod +x /mnt/install/mawlspack-install.sh
+chmod +x /mnt/install/aurapps.sh
 if [[ $insnumber = 1 ]]; then
 echo Installing Barebones...
 bones=`cat /tmp/apps/bones.list`
 arch-chroot /mnt pacman --noconfirm -S xorg-server xorg-xinit gnome-shell nautilus gnome-terminal gnome-tweaks gnome-control-center xdg-user-dirs gdm
-arch-chroot /mnt bash /tmp/install/mawlspack-install.sh
+arch-chroot /mnt bash /home/install/mawlspack-install.sh
 fi
 if [[ $insnumber = 2 ]]; then
 echo Installing Minimal...
 bones=`cat /tmp/apps/bones.list`
 minimal=`cat /tmp/apps/minimal.list`
 arch-chroot /mnt pacman --noconfirm -S xorg-server xorg-xinit gnome-shell nautilus gnome-terminal gnome-tweaks gnome-control-center xdg-user-dirs gdm firefox nano gedit wget curl gnome-backgrounds gnome-calculator gnome-screenshot gnome-system-monitor gnome-icon-theme
-arch-chroot /mnt bash /tmp/install/mawlspack-install.sh
+arch-chroot /mnt bash /home/install/mawlspack-install.sh
 fi
 if [[ $insnumber = 3 ]]; then
 echo Installing Standard...
 bones=`cat /tmp/apps/bones.list`
 minimal=`cat /tmp/apps/minimal.list`
 standard=`cat /tmp/apps/standard.list`
-arch-chroot /mnt pacman --noconfirm -S xorg-server xorg-xinit gnome-shell nautilus gnome-terminal gnome-tweaks gnome-control-center xdg-user-dirs gdm firefox nano gedit wget curl gnome-backgrounds gnome-calculator gnome-screenshot gnome-system-monitor gnome-icon-theme htop neofetch lm_sensors
-arch-chroot /mnt bash /tmp/install/mawlspack-install.sh
-arch-chroot /mnt bash /tmp/install/aurapps.sh
+arch-chroot /mnt pacman --noconfirm -S xorg-server xorg-xinit gnome-shell nautilus gnome-terminal gnome-tweaks gnome-control-center xdg-user-dirs gdm firefox nano gedit wget curl gnome-backgrounds gnome-calculator gnome-screenshot gnome-system-monitor gnome-icon-theme htop neofetch lm_sensors pavucontrol
+arch-chroot /mnt bash /home/install/mawlspack-install.sh
+arch-chroot /mnt bash /home/install/aurapps.sh
 fi
+rm -r /mnt/home/install
 echo Finished installing applications!
 echo
