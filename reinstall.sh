35252caeadb023f6096427592dba03e6509876b9
mawi <42104518+DEM0NAssissan7@users.noreply.github.com>
2021-01-02 09:22:58 -0600

    Reinstall script [for debugging]

---
+++ reinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo Welcome to the reinstall script!
+cp uninstall.sh /root
+chmod +x /root/uninstall.sh
+cd /root
+rm -r zerolinux
+git clone https://github.com/DEM0NAssissan7/zerolinux
+cd zerolinux
+chmod +x backend.sh
+echo Finished
