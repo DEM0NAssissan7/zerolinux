9618dd8ce222ea6aaa78193fed9ccca2433fe2d1
mawi <42104518+DEM0NAssissan7@users.noreply.github.com>
2021-01-02 11:32:52 -0600

    fixed the default entry

--- shells/bootloader.sh
+++ shells/bootloader.sh
@@ -1,5 +1,6 @@
 res3=y
 read -p "Do you wish to install the GRUB bootloader [In most cases, yes]: [Y/n] " res3
+if [[ $res3 = "" ]]; then res3=y;fi
 if [[ $res3 = y ]]; then
 echo Installing bootloader
 pacman -r /mnt --noconfirm -S efibootmgr grub
