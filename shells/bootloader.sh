c86493bb3f2cc384a53976a70d9db0ea41b29b9f
mawi <42104518+DEM0NAssissan7@users.noreply.github.com>
2021-01-02 10:27:33 -0600

    Fixed some stuff that killed the terminal

--- shells/bootloader.sh
+++ shells/bootloader.sh
@@ -2,8 +2,11 @@ res3=y
 read -p "Do you wish to install the GRUB bootloader [In most cases, yes]: [Y/n] " res3
 if [[ $res3 = y ]]; then
 echo Installing bootloader
-pacman -r /mnt --noconfirm -S efibootmgr
+pacman -r /mnt --noconfirm -S efibootmgr grub
 echo "grub-install $devins;grub-mkconfig -o /boot/grub/grub.cfg" | arch-chroot /mnt
 echo Finished installing the bootloader!
 fi
+echo Setting up startup services
+pacman -r /mnt --noconfirm -S networkmanager
+echo systemctl enable NetworkManager | arch-chroot /mnt
 exit
