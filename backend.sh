5ce1594ccbc963ad8f870435709c7a6f49fb81b3
mawi <42104518+DEM0NAssissan7@users.noreply.github.com>
2021-01-02 12:15:41 -0600

    removed the script at the beginning

--- backend.sh
+++ backend.sh
@@ -8,8 +8,6 @@ read -p "Press enter to continue..."
 echo
 rm -r /tmp/zerolinux
 cp -r ../zerolinux /tmp
-stty tostop
-nohup ../backend.sh </dev/null &
 
 #Chmodding the scripts
 chmod +x /tmp/zerolinux/nocrash.sh
