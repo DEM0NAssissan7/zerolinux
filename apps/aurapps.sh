23a831b3153cdcc5eb5a3d485274a329713b8f80
mawi <42104518+DEM0NAssissan7@users.noreply.github.com>
2021-01-02 14:08:52 -0600

    Delete aurapps.sh

--- apps/aurapps.sh
+++
@@ -1,9 +0,0 @@
-cd /tmp 
-git clone https://aur.archlinux.org/google-chrome.git
-cd google-chrome
-makepkg -si
-
-cd /tmp
-git clone https://aur.archlinux.org/gnome-shell-extension-appindicator.git
-cd gnome-shell-extension-appindicator
-makepkg -si
