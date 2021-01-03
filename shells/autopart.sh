94e4de1440a82a6847ff11fc30419813219af933
mawi <42104518+DEM0NAssissan7@users.noreply.github.com>
2021-01-03 11:59:02 -0600

    Add files via upload

---
+++ shells/autopart.sh
@@ -0,0 +1,39 @@
+echo WARNING: THIS WILL DELETE ALL OF YOUR DATA!
+read -p "Are you SURE you want to continue? [y/N] " res1
+if [[ res1 = "" ]];then res1=n;fi
+if [[ res1 = y ]];then
+#exec code
+echo Destroying data...
+if [[ /tmp/devins ]]; then devins=`cat /tmp/devins`; else devins=/dev/sda;fi
+dd if=/dev/zero of=$devins bs=512 count=1 conv=notrunc
+
+echo Detecting boot type
+if [[ /sys/firmware/efi ]]; then
+echo Detected EFI system
+system=efi
+else
+echo Detected BIOS system
+system=bios
+fi
+echo Creating partitions
+if [[ $system = efi ]];then
+sfdisk < /tmp/zerolinux/extras/efi.sfdisk
+echo Formatting partitions
+mkfs.fat -F 32 $devins"1"
+mkfs.ext4 -F $devins"2"
+echo Mounting partitions
+mount $devins"2" /mnt
+mkdir /mnt/boot/efi
+mount $devins"1" /mnt/boot/efi
+fi
+
+if [[ $system = bios ]];then
+sfdisk < /tmp/zerolinux/extras/bios.sfdisk
+echo Formatting partitions
+mkfs.ext4 -F $devins"1"
+mkfs.ext4 -F $devins"2"
+echo Mounting partitions
+mount $devins"2" /mnt
+mkdir /mnt/boot
+mount $devins"1" /mnt/boot
+fi
