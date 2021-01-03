7511388945ddaf6c32e06bb8956627f2de50ceb6
mawi <42104518+DEM0NAssissan7@users.noreply.github.com>
2021-01-03 16:20:19 -0600

    Added echos for each operation

--- shells/settings.sh
+++ shells/settings.sh
@@ -3,8 +3,10 @@ echo
 insnumber=`cat /tmp/insnumber.dat`
 username=`cat /tmp/username.dat`
 
-#Theme
 if [[ $insnumber = 2 || $insnumber = 3 ]]; then
+
+#Theme
+echo Applying theme settings
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set org.gnome.desktop.interface gtk-theme "mawlspack-theme"
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set org.gnome.desktop.interface icon-theme "Papirus"
@@ -12,25 +14,26 @@ arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set org.gnome.shell.extensions.user-theme name "mawlspack-shell"
 
 #Favorite Apps
+echo Setting favorite apps
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set org.gnome.shell favorite-apps ['firefox.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop']
 
 #Fonts
+echo Configuring fonts
 arch-chroot /mnt pacman --noconfirm -S freetype2 fontconfig cairo
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set org.gnome.desktop.interface font-name "Cantarell 11"
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set org.gnome.desktop.interface document-font-name "Sans 11"
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set org.gnome.desktop.interface monospace-font-name "Monospace 13"
-rm -r /home/install
+rm -r /mnt/home/install
 mkdir /mnt/home/$username/.config
 mkdir /mnt/home/$username/.config/fontconfig
 cp /tmp/zerolinux/gnome-configs/fonts.conf /mnt/home/$username/.config/fontconfig/
 
-#Locales
-arch-chroot /mnt localedef -f UTF-8 -i en_US en_US.UTF-8
-
 #Minimize, Maximize
+echo Configuring Minimize and Maximize
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
 
 #Zsh
+echo Configuring ZSH
 mkdir /mnt/home/install/
 cp /tmp/zerolinux/gnome-configs/terminal-profile.dconf /mnt/home/install/
 arch-chroot /mnt sudo -u $username dconf dump /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ < home/install/terminal-profile.dconf
@@ -41,14 +44,23 @@ arch-chroot /mnt chsh -s $(which zsh) $(whoami)
 
 fi
 
-#Mouse Sensitivity
+#Locales
+echo Configuring GNOME locales
+arch-chroot /mnt localedef -f UTF-8 -i en_US en_US.UTF-8
+
+#Mouse Acceleration
+echo Adjusting mouse settings
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'flat'
 
 #Nautilus
+echo Configuring Nautilus
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set org.gnome.nautilus.preferences show-hidden-files true
 
 # Ctrl-Alt-T Terminal Shortcut
+echo Configuring Terminal Shortcut
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Terminal'
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'gnome-terminal'
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Ctrl><Alt>t'
+echo
+echo Finished applying settings!
