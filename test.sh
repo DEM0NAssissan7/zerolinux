130c2b00016c99d52ed435e12056ab7b303b9bc0
mawi <42104518+DEM0NAssissan7@users.noreply.github.com>
2021-01-02 20:17:54 -0600

    Add files via upload

--- test.sh
+++ test.sh
@@ -7,5 +7,7 @@ cp /tmp/zerolinux/extras/aurapps.sh /mnt/home/install/
 chmod +x /mnt/install/mawlspack-install.sh
 chmod +x /mnt/install/aurapps.sh
 
+useradd -R /mnt -g wheel admin
+
 #arch-chroot /mnt bash /home/install/mawlspack-install.sh
 arch-chroot /mnt bash /home/install/aurapps.sh
