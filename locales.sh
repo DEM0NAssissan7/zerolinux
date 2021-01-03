52aa243f28b11964acc69905bd188ac0a3d1fda4
mawi <42104518+DEM0NAssissan7@users.noreply.github.com>
2021-01-03 12:19:22 -0600

    Delete locales.sh

--- locales.sh
+++
@@ -1,5 +0,0 @@
-echo Configuring locales...
-echo en_US.UTF-8 UTF-8 >> /mnt/etc/locale.gen
-echo en_US ISO-8859-1 >> /mnt/etc/locale.gen
-echo LANG=en-US.UTF-8 > /mnt/etc/locale.conf
-arch-chroot /mnt locale-gen
