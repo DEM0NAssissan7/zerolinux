c5c680b2654f6368e9492e4e17f5b1a02c6a4e78
mawi <42104518+DEM0NAssissan7@users.noreply.github.com>
2021-01-05 13:02:37 -0600

    Add files via upload

--- shells/settings.sh
+++ shells/settings.sh
@@ -50,9 +50,9 @@ arch-chroot /mnt sh /home/install/omz.sh
 cat /tmp/zerolinux/gnome-configs/zshrc > /mnt/home/$username/.zshrc
 #cp -r /mnt/home/$username/.oh-my-zsh /root/
 cp /mnt/etc/sudoers /tmp/
-echo "$username ALL=(ALL) NOPASSWD: ALL" >> /mnt/etc/sudoers 
+echo "$username   ALL=(ALL) NOPASSWD: ALL" >> /mnt/etc/sudoers
 
-arch-chroot /mnt sudo -u $username chsh -s $(which zsh)
+arch-chroot /mnt sudo -u $username chsh -s /usr/bin/zsh
 
 mv /tmp/sudoers /mnt/etc/sudoers
 
