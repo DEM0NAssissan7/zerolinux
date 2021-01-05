56adcffb52b96be035e890249e494eae992a4f61
mawi <42104518+DEM0NAssissan7@users.noreply.github.com>
2021-01-04 21:51:12 -0600

    Add files via upload

--- extras/aurapps.sh
+++ extras/aurapps.sh
@@ -4,18 +4,21 @@ cp /tmp/zerolinux/extras/sudoers /mnt/etc/sudoers
 git clone https://aur.archlinux.org/google-chrome.git
 mkdir /mnt/home/install
 mv google-chrome /mnt/home/install/
+chmod -R 777 /mnt/home/install/
 echo "cd /home/install/google-chrome && sudo -u admin makepkg -si" | arch-chroot /mnt 
 rm -r /mnt/home/install
 
 git clone https://aur.archlinux.org/gnome-shell-extension-appindicator.git
 mkdir /mnt/home/install
 mv gnome-shell-extension-appindicator /mnt/home/install/
+chmod -R 777 /mnt/home/install/
 echo "cd /home/install/gnome-shell-extension-appindicator && sudo -u admin makepkg -si" | arch-chroot /mnt 
 rm -r /mnt/home/install
 
 git clone https://aur.archlinux.org/gnome-shell-extension-desktop-icons-ng.git
 mkdir /mnt/home/install
 mv gnome-shell-extension-desktop-icons-ng /mnt/home/install/
+chmod -R 777 /mnt/home/install/
 echo "cd /home/install/gnome-shell-extension-desktop-icons-ng && sudo -u admin makepkg -si" | arch-chroot /mnt 
 rm -r /mnt/home/install
 
