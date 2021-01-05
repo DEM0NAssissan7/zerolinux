aeb182d6e26a9a35ca77a0e185c3c352216d9fbc
mawi <42104518+DEM0NAssissan7@users.noreply.github.com>
2021-01-05 12:46:16 -0600

    fixed omzsh and made general improvements

--- shells/settings.sh
+++ shells/settings.sh
@@ -25,13 +25,13 @@ arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set
 
 #Fonts
 echo Configuring fonts
-arch-chroot /mnt pacman --noconfirm -Sq freetype2 fontconfig cairo
+#arch-chroot /mnt pacman --noconfirm -Sq freetype2 fontconfig cairo
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set org.gnome.desktop.interface font-name "Cantarell 11"
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set org.gnome.desktop.interface document-font-name "Sans 11"
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set org.gnome.desktop.interface monospace-font-name "Monospace 13"
-mkdir /mnt/home/$username/.config
-mkdir /mnt/home/$username/.config/fontconfig
-cp /tmp/zerolinux/gnome-configs/fonts.conf /mnt/home/$username/.config/fontconfig/
+#mkdir /mnt/home/$username/.config
+#mkdir /mnt/home/$username/.config/fontconfig
+#cp /tmp/zerolinux/gnome-configs/fonts.conf /mnt/home/$username/.config/fontconfig/
 
 #Minimize, Maximize
 echo Configuring Minimize and Maximize
@@ -41,20 +41,25 @@ arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set
 echo Configuring ZSH
 mkdir /mnt/home/install/
 
-cp /tmp/zerolinux/gnome-configs/terminal-profile.dconf /mnt/home/install/
-arch-chroot /mnt sudo -u $username dconf load /org/gnome/terminal/legacy/profiles:/ < /home/install/terminal-profile.dconf
+#cp /tmp/zerolinux/gnome-configs/terminal-profile.dconf /mnt/home/install/
+#arch-chroot /mnt sudo -u $username dconf load /org/gnome/terminal/legacy/profiles:/ < /home/install/terminal-profile.dconf
 
 cp /tmp/zerolinux/gnome-configs/omz.sh /mnt/home/install/
-arch-chroot /mnt pacman -Syu git
-arch-chroot /mnt sudo -u $username sh /home/install/omz.sh
+arch-chroot /mnt pacman --noconfirm -Syu git
+arch-chroot /mnt sh /home/install/omz.sh
 cat /tmp/zerolinux/gnome-configs/zshrc > /mnt/home/$username/.zshrc
-cp -r /mnt/home/$username/.oh-my-zsh /root/
-arch-chroot /mnt chsh -s $(which zsh) $(whoami)
+#cp -r /mnt/home/$username/.oh-my-zsh /root/
+cp /mnt/etc/sudoers /tmp/
+echo "$username ALL=(ALL) NOPASSWD: ALL" >> /mnt/etc/sudoers 
+
+arch-chroot /mnt sudo -u $username chsh -s $(which zsh) $(whoami)
+
+mv /tmp/sudoers /mnt/etc/sudoers
 
 fi
 
 #Locales
-echo Configuring GNOME locales
+echo Configuring locales
 arch-chroot /mnt localedef -f UTF-8 -i en_US en_US.UTF-8
 
 #Battery Icon
@@ -80,5 +85,6 @@ arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Terminal'
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'gnome-terminal'
 arch-chroot /mnt sudo -u $username dbus-launch --exit-with-session gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Ctrl><Alt>t'
+
 echo
 echo Finished applying settings!
